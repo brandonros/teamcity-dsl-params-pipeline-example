2148ea5c9694386857eafd57e20d4b60dbf4ea2c
Brandon Ros <brandonros1@gmail.com>
2023-03-07 15:16:05 -0500

    outline example RE pipeline

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,3 +1,5 @@
+// documentation: https://build.bitcoinabc.org/app/dsl-documentation/jetbrains.build-server.configs.kotlin.v2019_2/-build-steps/index.html
+
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
@@ -28,17 +30,19 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2019.2"
 
 project {
-
     vcsRoot(Sources)
 
     buildType(Build)
-    buildType(Test)
 
     sequential {
         buildType(Build)
-        buildType(Test, options = { runOnSameAgent = true })
     }
 }
+
+object Sources : GitVcsRoot({
+    name = "Sources"
+    url = DslContext.getParameter("repoUrl")
+})
 
 object Build : BuildType({
     name = "Build"
@@ -50,36 +54,81 @@ object Build : BuildType({
     }
 
     steps {
-        maven {
-            goals = "package"
-            runnerArgs = "-DskipTests"
+        script {
+            name = "001-dotnet clean"
+            scriptContent = """
+echo "Hello, world!"
+""".trimIndent()
+        }
+
+        script {
+            name = "002-dotnet build"
+            scriptContent = """
+echo "Hello, world!"
+""".trimIndent()
+        }
+
+        script {
+            name = "003-dotnet unit test"
+            scriptContent = """
+echo "Hello, world!"
+""".trimIndent()
+        }
+
+        script {
+            name = "004-remove read-only attribute from web.config prior to dotnet publish"
+            scriptContent = """
+echo "Hello, world!"
+""".trimIndent()
+        }
+
+        script {
+            name = "005-dotnet publish (to artifactory)"
+            scriptContent = """
+echo "Hello, world!"
+""".trimIndent()
+        }
+
+        script {
+            name = "006-resharper inspections"
+            scriptContent = """
+echo "Hello, world!"
+""".trimIndent()
+        }
+
+        script {
+            name = "007-run tests"
+            scriptContent = """
+echo "Hello, world!"
+""".trimIndent()
+        }
+
+        script {
+            name = "008-fortify scan"
+            scriptContent = """
+echo "Hello, world!"
+""".trimIndent()
+        }
+
+        script {
+            name = "009-octo pack"
+            scriptContent = """
+echo "Hello, world!"
+""".trimIndent()
+        }
+
+        script {
+            name = "010-octopus create release and deploy"
+            scriptContent = """
+echo "Hello, world!"
+""".trimIndent()
+        }
+
+        script {
+            name = "011-sonarqube scan"
+            scriptContent = """
+echo "Hello, world!"
+""".trimIndent()
         }
     }
-})
-
-object Test : BuildType({
-    name = "Test"
-    buildNumberPattern = "${Build.depParamRefs.buildNumber}"
-
-    vcs {
-        root(Sources)
-    }
-
-    steps {
-        maven {
-            goals = "test"
-        }
-    }
-
-    triggers {
-        vcs {
-            branchFilter = ""
-        }
-    }
-
-})
-
-object Sources : GitVcsRoot({
-    name = "Sources"
-    url = DslContext.getParameter("repoUrl")
 })
