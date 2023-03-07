73e2671780870a8ff3cdce99a02118efd58c5b61
Brandon Ros <brandonros1@gmail.com>
2023-03-07 15:22:39 -0500

    no artifact needed?

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,35 +5,11 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2019.2"
 
 project {
     vcsRoot(Sources)
-
     buildType(Build)
-
     sequential {
         buildType(Build)
     }
@@ -46,8 +22,6 @@ object Sources : GitVcsRoot({
 
 object Build : BuildType({
     name = "Build"
-
-    artifactRules = DslContext.getParameter("artifactPath")
 
     vcs {
         root(Sources)
