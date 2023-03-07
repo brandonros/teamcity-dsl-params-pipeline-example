ade95cbfb3726d5107f11b05f43b9cce035d2676
Brandon Ros <brandonros1@gmail.com>
2023-03-07 15:19:19 -0500

    maven -> script

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,7 @@
 // documentation: https://build.bitcoinabc.org/app/dsl-documentation/jetbrains.build-server.configs.kotlin.v2019_2/-build-steps/index.html
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
