e6338ca13bb02e53eaa828b9de413a18be0a59f6
Brandon Ros <brandonros1@gmail.com>
2023-03-08 14:18:51 -0500

    no _root package on .kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,5 @@
 // documentation: https://build.bitcoinabc.org/app/dsl-documentation/jetbrains.build-server.configs.kotlin.v2019_2/-build-steps/index.html
 
-package _Root
-
 import _Root.CustomProject
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
