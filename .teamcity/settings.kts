3f014e968cab0eb30e8b130d66e6b81587e3f274
Brandon Ros <brandonros1@gmail.com>
2023-03-08 13:06:52 -0500

    GitVcsRoot

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,7 +5,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
-fun createSources(): Sources {
+fun createSources(): GitVcsRoot {
     val sources = GitVcsRoot({
         name = "Sources"
         url = DslContext.getParameter("repoUrl")
