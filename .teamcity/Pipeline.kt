29b9da0a5c007c5dad63f97da30613abf42305f9
Brandon Ros <brandonros1@gmail.com>
2023-03-08 14:10:50 -0500

    almost got it

---
+++ .teamcity/Pipeline.kt
@@ -0,0 +1,85 @@
+package _Root
+
+import _Root.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+
+object Pipeline : BuildType({
+    name = "Pipeline"
+
+    vcs {
+        root(Sources)
+    }
+
+    steps {
+        script {
+            name = "001-dotnet clean"
+            scriptContent = """
+cd src/
+dotnet clean
+""".trimIndent()
+        }
+
+        script {
+            name = "002-dotnet build"
+            scriptContent = """
+cd src/
+dotnet build
+""".trimIndent()
+        }
+
+        script {
+            name = "003-dotnet test"
+            scriptContent = """
+cd src/
+dotnet test
+""".trimIndent()
+        }
+
+        script {
+            name = "004-dotnet publish"
+            scriptContent = """
+cd src/
+dotnet publish
+echo "TODO: push to artifactory?"
+""".trimIndent()
+        }
+
+        script {
+            name = "005-resharper inspections"
+            scriptContent = """
+echo "TODO"
+""".trimIndent()
+        }
+
+        script {
+            name = "006-fortify scan"
+            scriptContent = """
+echo "TODO"
+""".trimIndent()
+        }
+
+        script {
+            name = "007-octo pack"
+            scriptContent = """
+echo "TODO"
+""".trimIndent()
+        }
+
+        script {
+            name = "008-octopus create release and deploy"
+            scriptContent = """
+echo "TODO"
+""".trimIndent()
+        }
+
+        script {
+            name = "009-sonarqube scan"
+            scriptContent = """
+echo "TODO"
+""".trimIndent()
+        }
+    }
+})
--- .teamcity/Project.kt
+++ .teamcity/Project.kt
@@ -1,5 +1,3 @@
-// documentation: https://build.bitcoinabc.org/app/dsl-documentation/jetbrains.build-server.configs.kotlin.v2019_2/-build-steps/index.html
-
 package _Root
 
 import _Root.*
@@ -8,5 +6,11 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
-version = "2019.2"
-project(_Root.Project)
+object Project : Project({
+    name = "Project"
+    vcsRoot(Sources)
+    buildType(Pipeline)
+    sequential {
+        buildType(CustomBuildType)
+    }
+})
