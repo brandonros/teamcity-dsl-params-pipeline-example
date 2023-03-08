96da2e0af2ac636aff8c30f56cb9ccfd5dac2470
Brandon Ros <brandonros1@gmail.com>
2023-03-08 13:52:53 -0500

    refactor

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,8 +4,93 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
-import _Self.VcsRoot
-import _Self.Pipeline
+
+class CustomVcsRoot : GitVcsRoot() {
+    init {
+        id("VcsRoot")
+        name = "VcsRoot"
+        url = DslContext.getParameter("repoUrl")
+    }
+}
+
+class CustomPipeline(vcsRoot: CustomVcsRoot): BuildType() {
+    init {
+        id("CustomPipeline")
+        name = "CustomPipeline"
+        vcs = {
+            root(vcsRoot)
+        }
+        steps = {
+            script {
+                name = "001-dotnet clean"
+                scriptContent = """
+    cd src/
+    dotnet clean
+    """.trimIndent()
+            }
+
+            script {
+                name = "002-dotnet build"
+                scriptContent = """
+    cd src/
+    dotnet build
+    """.trimIndent()
+            }
+
+            script {
+                name = "003-dotnet test"
+                scriptContent = """
+    cd src/
+    dotnet test
+    """.trimIndent()
+            }
+
+            script {
+                name = "004-dotnet publish"
+                scriptContent = """
+    cd src/
+    dotnet publish
+    echo "TODO: push to artifactory?"
+    """.trimIndent()
+            }
+
+            script {
+                name = "005-resharper inspections"
+                scriptContent = """
+    echo "TODO"
+    """.trimIndent()
+            }
+
+            script {
+                name = "006-fortify scan"
+                scriptContent = """
+    echo "TODO"
+    """.trimIndent()
+            }
+
+            script {
+                name = "007-octo pack"
+                scriptContent = """
+    echo "TODO"
+    """.trimIndent()
+            }
+
+            script {
+                name = "008-octopus create release and deploy"
+                scriptContent = """
+    echo "TODO"
+    """.trimIndent()
+            }
+
+            script {
+                name = "009-sonarqube scan"
+                scriptContent = """
+    echo "TODO"
+    """.trimIndent()
+            }
+        }
+    }
+}
 
 version = "2019.2"
 
@@ -14,9 +99,12 @@ project {
         param("teamcity.ui.settings.readOnly", "true")
     }
 
-    vcsRoot(VcsRoot)
-    buildType(Pipeline)
+    let vcsRoot = CustomVcsRoot()
+    let pipeline = CustomPipeline(vcsRoot)
+
+    vcsRoot(vcsRoot)
+    buildType(pipeline)
     sequential {
-        buildType(Pipeline)
+        buildType(pipeline)
     }
 }
