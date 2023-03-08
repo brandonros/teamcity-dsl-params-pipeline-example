32376ecbcc91cb7a0939a07a658e4d766dcaad05
Brandon Ros <brandonros1@gmail.com>
2023-03-08 15:51:42 -0500

    pull in as package from nexus attempt 1

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,10 +1,6 @@
 // documentation: https://build.bitcoinabc.org/app/dsl-documentation/jetbrains.build-server.configs.kotlin.v2019_2/-build-steps/index.html
 
-import _Root.CustomProject
+import com.brandonros.test.CustomProject
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
-
 version = "2019.2"
-project(_Root.CustomProject)
+project(CustomProject)
