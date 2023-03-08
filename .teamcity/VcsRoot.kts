2ac6e17e79d73ba48a5555b92394444c434ae91b
Brandon Ros <brandonros1@gmail.com>
2023-03-08 13:16:17 -0500

    imports

--- .teamcity/VcsRoot.kts
+++ .teamcity/VcsRoot.kts
@@ -1,5 +1,8 @@
 package _Self
 
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 object VcsRoot : GitVcsRoot({
