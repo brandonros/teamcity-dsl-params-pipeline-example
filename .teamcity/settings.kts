becf31b72787261fa02aad713df72fd19c2f93ec
Brandon Ros <brandonros1@gmail.com>
2023-03-08 12:58:36 -0500

    try to make pipeline a variable then a function them an importable library package

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -8,92 +8,96 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 version = "2019.2"
 
 project {
+    params {
+        param("teamcity.ui.settings.readOnly", "true")
+    }
+    
+    val Sources = GitVcsRoot({
+        name = "Sources"
+        url = DslContext.getParameter("repoUrl")
+    })
+
+    val Pipeline = BuildType({
+        name = "Pipeline"
+
+        vcs {
+            root(sources)
+        }
+
+        steps {
+            script {
+                name = "001-dotnet clean"
+                scriptContent = """
+    cd src/
+    dotnet clean
+    """.trimIndent()
+            }
+
+            script {
+                name = "002-dotnet build"
+                scriptContent = """
+    cd src/
+    dotnet build
+    """.trimIndent()
+            }
+
+            script {
+                name = "003-dotnet test"
+                scriptContent = """
+    cd src/
+    dotnet test
+    """.trimIndent()
+            }
+
+            script {
+                name = "004-dotnet publish"
+                scriptContent = """
+    cd src/
+    dotnet publish
+    echo "TODO: push to artifactory?"
+    """.trimIndent()
+            }
+
+            script {
+                name = "005-resharper inspections"
+                scriptContent = """
+    echo "TODO"
+    """.trimIndent()
+            }
+
+            script {
+                name = "006-fortify scan"
+                scriptContent = """
+    echo "TODO"
+    """.trimIndent()
+            }
+
+            script {
+                name = "007-octo pack"
+                scriptContent = """
+    echo "TODO"
+    """.trimIndent()
+            }
+
+            script {
+                name = "008-octopus create release and deploy"
+                scriptContent = """
+    echo "TODO"
+    """.trimIndent()
+            }
+
+            script {
+                name = "009-sonarqube scan"
+                scriptContent = """
+    echo "TODO"
+    """.trimIndent()
+            }
+        }
+    })
+
     vcsRoot(Sources)
-    buildType(Build)
+    buildType(Pipeline)
     sequential {
-        buildType(Build)
+        buildType(Pipeline)
     }
 }
-
-object Sources : GitVcsRoot({
-    name = "Sources"
-    url = DslContext.getParameter("repoUrl")
-})
-
-object Build : BuildType({
-    name = "Build"
-
-    vcs {
-        root(Sources)
-    }
-
-    steps {
-        script {
-            name = "001-dotnet clean"
-            scriptContent = """
-cd src/
-dotnet clean
-""".trimIndent()
-        }
-
-        script {
-            name = "002-dotnet build"
-            scriptContent = """
-cd src/
-dotnet build
-""".trimIndent()
-        }
-
-        script {
-            name = "003-dotnet test"
-            scriptContent = """
-cd src/
-dotnet test
-""".trimIndent()
-        }
-
-        script {
-            name = "004-dotnet publish"
-            scriptContent = """
-cd src/
-dotnet publish
-echo "TODO: push to artifactory?"
-""".trimIndent()
-        }
-
-        script {
-            name = "005-resharper inspections"
-            scriptContent = """
-echo "TODO"
-""".trimIndent()
-        }
-
-        script {
-            name = "006-fortify scan"
-            scriptContent = """
-echo "TODO"
-""".trimIndent()
-        }
-
-        script {
-            name = "007-octo pack"
-            scriptContent = """
-echo "TODO"
-""".trimIndent()
-        }
-
-        script {
-            name = "008-octopus create release and deploy"
-            scriptContent = """
-echo "TODO"
-""".trimIndent()
-        }
-
-        script {
-            name = "009-sonarqube scan"
-            scriptContent = """
-echo "TODO"
-""".trimIndent()
-        }
-    }
-})
